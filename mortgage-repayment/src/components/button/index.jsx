ff78ae282be0cddf25cab788cd8bc72a21201dea
João Paulo <jp.cm@hotmail.com>
2024-11-17 21:03:46 -0400

    feat: button repayment

---
+++ mortgage-repayment/src/components/button/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+const Button = () => {
+  return (
+    <button>  
+      Calculate Repayments
+    </button>
+  );
+};
+export default Button;
--- mortgage-repayment/src/components/calculator/index.jsx
+++ mortgage-repayment/src/components/calculator/index.jsx
@@ -1,49 +1,56 @@
-import './styles.css'
+import Button from "../button";
+import "./styles.css";
+
 const Calculator = () => {
-    return <div className="container-calculator">
-        <div className='title'>
-            <h2>Mortgage Calculator</h2>
-            <p>Clear All</p>
+  return (
+    <div className="container-calculator">
+      <div className="title">
+        <h2>Mortgage Calculator</h2>
+        <p>Clear All</p>
+      </div>
+      <div>
+        <p>Mortgage Amount</p>
+        <div className="input-amount">
+          <p>&pound;</p> <input type="text" placeholder="$3000" />
+        </div>
+      </div>
+      <div className="input-term-and-rate">
+        <div>
+          <p>Mortgage Term</p>
+          <div className="input-term">
+            <input type="text" placeholder="25" />
+            <p>years</p>
+          </div>
         </div>
         <div>
-            <p>Mortgage Amount</p>
-            <div className='input-amount'>
-                <p>&pound;</p> <input type="text" placeholder="$3000" />
-            </div>
+          <p>Interest Rate</p>
+          <div className="input-rate">
+            <input type="text" placeholder="5.25" />
+            <p>%</p>
+          </div>
         </div>
-        <div className='input-term-and-rate'>
-            <div>
-                <p>Mortgage Term</p>
-                <div className='input-term'>
-                    <input type="text" placeholder="25" />
-                    <p>years</p>
-                </div>
-            </div>
-            <div>
-                <p>Interest Rate</p>
-                <div className='input-rate'>
+      </div>
 
-                    <input type="text" placeholder="5.25" />
-                    <p>%</p>
-                </div>
-
-            </div>
+      <div className="input-repayment-and-interest">
+        <p>Mortgage Type</p>
+        <div className="wrap-input">
+          <input
+            type="radio"
+            id="repayment"
+            value="repayment"
+            className="input-repayment"
+          />
+          <label for="repayment">Repayment</label>
         </div>
+        <div className="wrap-input">
+          <input type="radio" id="interest" value="Interest Only" />
+          <label for="interest">Interest Only</label>
+        </div>
+      </div>
 
-  {/*       <div className='input-repayment-and-interest'>
-            <p>Mortgage Type</p>
-            <div className='wrap-input'>
-                <input type="radio" id="repayment" value="repayment" className='input-repayment' />
-                <label for="repayment">Repayment</label>
-            </div>
-            <div className='wrap-input'>
-                <input type="radio" id='interest' value="Interest Only" />
-                <label for="interest">Interest Only</label>
-            </div>
-
-        </div> */}
-        <button>Calculate Repayments</button>
+      <Button /> 
     </div>
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
